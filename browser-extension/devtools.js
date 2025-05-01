7f327a5c18bdb9c2b8b928d010b901495fdddeab
Christian Johansen <christian@cjohansen.no>
2025-05-01 14:00:11 +0200

    Keep extension working after the inspected page is reloaded

--- browser-extension/devtools.js
+++ browser-extension/devtools.js
@@ -1,6 +1,14 @@
-chrome.devtools.panels.create("Dataspex", "" /* icon */, "panel.html", function (panel) {
+function injectContentScript() {
   chrome.scripting.executeScript({
     target: { tabId: chrome.devtools.inspectedWindow.tabId },
     files: ["content-script.js"]
+  });
+}
+
+chrome.devtools.panels.create("Dataspex", "" /* icon */, "panel.html", function (panel) {
+  injectContentScript();
+
+  chrome.devtools.network.onNavigated.addListener(function () {
+    injectContentScript();
   });
 });
