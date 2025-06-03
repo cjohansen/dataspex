f4cbdb2647826fae32bf3f98471ae3346ab360fd
Christian Johansen <christian@cjohansen.no>
2025-06-03 20:10:48 +0200

    Move the demo state out of the dev setup

---
+++ dev/dataspex/demo_state.cljs
@@ -0,0 +1,49 @@
+(ns dataspex.demo-state)
+
+(defonce app-state
+  {:app/title "Movie Explorer"
+   :user {:id 42
+          :name "Ada Lovelace"
+          :roles #{:admin :reviewer}
+          :auth-token "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQyfQ.sEcrEt-KU5phTcLBBjDDxpsHloA4j1ebJfXnB4rmkmc"
+          :last-login (js/Date. "2025-04-15T12:34:56Z")}
+
+   :filters {:genres [:drama :sci-fi :comedy]
+             :year-range {:min 1990 :max 2024}
+             :search-term ""}
+
+   :ui {:modal {:type :movie/details
+                :movie-id 101}
+        :notifications [{:id 1 :type :info :message "Welcome, Ada!"}
+                        {:id 2 :type :error :message "Could not fetch recommendations"}]
+        :pagination {:current 2 :per-page 10}}
+
+   :movies [(with-meta
+              {:id 101
+               :title "Interstellar"
+               :year 2014
+               :genres [:sci-fi :drama]
+               :rating 8.6
+               :cast ["Matthew McConaughey" "Anne Hathaway"]}
+              {:favorited? true
+               :watchlist? false})
+
+            (with-meta
+              {:id 102
+               :title "Amélie"
+               :year 2001
+               :genres [:comedy :romance]
+               :rating 8.3
+               :cast ["Audrey Tautou"]}
+              {:favorited? false
+               :watchlist? true})]
+
+   :debug {:expanded-paths #{[:movies 0 :cast]
+                             [:ui :modal]}
+           :watch-history (mapv (fn [i] {:ts (+ 1713200000 i)
+                                         :value (rand-nth [true false nil])})
+                                (range 20))
+           :raw-js-array (js/Array. 1 2 3 "four" true {:x 5})
+           :raw-js-object (js-obj "a" 1 "b" (clj->js {:nested [1 2 3]}))}
+
+   :token "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30"})
--- dev/dataspex/dev.cljs
+++ dev/dataspex/dev.cljs
@@ -3,6 +3,7 @@
             [dataspex.data :as data]
             [dataspex.datascript :as datascript]
             [dataspex.demo-data :as demo-data]
+            [dataspex.demo-state :as demo-state]
             [dataspex.in-process-client :as in-process-client]
             [dataspex.inspector :as inspector]
             [dataspex.jwt :as jwt]
@@ -10,52 +11,6 @@
             [dataspex.render-client :as rc]))
 
 ::datascript/keep
-
-(defonce app-state
-  {:app/title "Movie Explorer"
-   :user {:id 42
-          :name "Ada Lovelace"
-          :roles #{:admin :reviewer}
-          :auth-token "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQyfQ.sEcrEt-KU5phTcLBBjDDxpsHloA4j1ebJfXnB4rmkmc"
-          :last-login (js/Date. "2025-04-15T12:34:56Z")}
-
-   :filters {:genres [:drama :sci-fi :comedy]
-             :year-range {:min 1990 :max 2024}
-             :search-term ""}
-
-   :ui {:modal {:type :movie/details
-                :movie-id 101}
-        :notifications [{:id 1 :type :info :message "Welcome, Ada!"}
-                        {:id 2 :type :error :message "Could not fetch recommendations"}]
-        :pagination {:current 2 :per-page 10}}
-
-   :movies [(with-meta
-              {:id 101
-               :title "Interstellar"
-               :year 2014
-               :genres [:sci-fi :drama]
-               :rating 8.6
-               :cast ["Matthew McConaughey" "Anne Hathaway"]}
-              {:favorited? true
-               :watchlist? false})
-
-            (with-meta
-              {:id 102
-               :title "Amélie"
-               :year 2001
-               :genres [:comedy :romance]
-               :rating 8.3
-               :cast ["Audrey Tautou"]}
-              {:favorited? false
-               :watchlist? true})]
-
-   :debug {:expanded-paths #{[:movies 0 :cast]
-                             [:ui :modal]}
-           :watch-history (mapv (fn [i] {:ts (+ 1713200000 i)
-                                         :value (rand-nth [true false nil])})
-                                (range 20))
-           :raw-js-array (js/Array. 1 2 3 "four" true {:x 5})
-           :raw-js-object (js-obj "a" 1 "b" (clj->js {:nested [1 2 3]}))}})
 
 (defonce store (atom {}))
 (data/add-string-inspector! jwt/inspect-jwt)
@@ -69,21 +24,22 @@
 (defn ^:dev/after-load main []
   (swap! store assoc ::loaded (.getTime (js/Date.))))
 
-(dataspex/inspect "App state" app-state)
+;;(dataspex/inspect "App state" demo-state/app-state)
+
+(inspector/inspect store "App state" demo-state/app-state)
 
 (comment
   (dataspex/inspect "DB" demo-data/conn)
 
-  (inspector/inspect store "App state" app-state)
   (inspector/inspect store "DB" demo-data/conn)
   (inspector/inspect store "Hiccup" (panel/render-panel @store "App state"))
 
   (js/setTimeout
-   #(dataspex/inspect  "App state" (assoc-in app-state [:filters :search-term] "batman"))
+   #(dataspex/inspect  "App state" (assoc-in demo-state/app-state [:filters :search-term] "batman"))
    500)
 
   (js/setTimeout
-   #(dataspex/inspect  "App state" (-> app-state
+   #(dataspex/inspect  "App state" (-> demo-state/app-state
                                        (update-in [:filters :genres] (fn [gs] (vec (next gs))))
                                        (assoc-in [:filters :search-term] "batman")))
    2500))
